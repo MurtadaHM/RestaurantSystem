b6b068f50ecba10528993ea440ba8231b3fcb8d9
Murtada Hussein <mmortada721@gmail.com>
2026-05-06 21:22:52 +0300

    Update PublicOrderSettings.cs

--- RestaurantSystem.Application/Configurations/PublicOrderSettings.cs
+++ RestaurantSystem.Application/Configurations/PublicOrderSettings.cs
@@ -4,6 +4,7 @@ namespace RestaurantSystem.Application.Configurations
 {
     public class PublicOrderSettings
     {
-        public Guid FallbackUserId { get; set; }
+        public Guid FallbackUserId { get; set; } =
+            Guid.Parse("eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee");
     }
 }
